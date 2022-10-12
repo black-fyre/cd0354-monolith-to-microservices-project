7cb32da661c49a78f98530dd51219f47d7346b18
Dahunsi Fehintoluwa <dahunsi.fehinti@gmail.com>
2022-10-12 23:15:52 +0100

    Add kubernetes details and screenshots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a469bee1789304fc692d4a1b4bb93c32-1402776555.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
